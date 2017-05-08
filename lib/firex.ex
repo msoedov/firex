43ee4e10701c98166105ee599fc663d2c5452219
Alex Myasoedov <msoedov@gmail.com>
2017-05-08 18:15:06 -0400

    Fix ident

--- lib/firex.ex
+++ lib/firex.ex
@@ -1,85 +1,85 @@
 defmodule Firex do
 
-    defmacro __using__(_opts) do
-      quote do
-        import Firex
+  defmacro __using__(_opts) do
+    quote do
+      import Firex
 
-        @before_compile Firex
-        @on_definition {Firex, :on_def}
-        IO.inspect __ENV__.module
+      @before_compile Firex
+      @on_definition {Firex, :on_def}
+      IO.inspect __ENV__.module
 
-        defmodule Main do
-            defdelegate main(args), to: __ENV__.module
+      defmodule Main do
+        defdelegate main(args), to: __ENV__.module
+      end
+
+      def main(args) do
+        params =  what_defined |> Enum.map(&opt_pair/1)
+        parsed = OptionParser.parse(args, List.first(params))
+        # IO.inspect parsed
+        case parsed do
+          {[verbose: true], [cmd], _} -> :do_verbose_thing
+          {_, [cmd], _} -> :do_thing
+          _ -> :help
         end
+      end
 
-        def main(args) do
-            params =  what_defined |> Enum.map(&opt_pair/1)
-            parsed = OptionParser.parse(args, List.first(params))
-            IO.inspect parsed
-            case parsed do
-            	{[verbose: true], [cmd], _} -> :do_verbose_thing
-            	{_, [cmd], _} -> :do_thing
-            	_ -> :help
-            end
-        end
+    end
+  end
 
+  defmacro __before_compile__(_) do
+    quote do
+      def what_defined do
+        @commands
       end
     end
+  end
 
-    defmacro __before_compile__(_) do
-        quote do
-            def what_defined do
-                @commands
-            end
-        end
-    end
+  def on_def(_env, _kind, :dispatch, _args, _guards, _body) do
+  end
+  def on_def(env, :def, name, args, guards, _body) do
+    module = env.module
+    defs = Module.get_attribute(module, :commands) || []
+    Module.put_attribute(module, :commands, [{name, args, guards} | defs])
+  end
+  def on_def(_env, _kind, _name, _args, _guards, _body) do
+  end
 
-    def on_def(_env, _kind, :dispatch, _args, _guards, _body) do
-    end
-    def on_def(env, :def, name, args, guards, _body) do
-      module = env.module
-      defs = Module.get_attribute(module, :commands) || []
-      Module.put_attribute(module, :commands, [{name, args, guards} | defs])
-    end
-    def on_def(_env, _kind, _name, _args, _guards, _body) do
-    end
+  def opt_pair({_name, args, _guards}) do
+    switches = args
+    |> Enum.map(&Firex.arg_name/1)
+    |> Enum.map(fn name -> {name, :string} end)
+    |> Enum.into(Keyword.new)
+    aliases = switches
+    |> Enum.map(fn {k, _} -> {Atom.to_string(k)
+    |> String.at(0)
+    |> String.to_atom(), k} end)
+    |> Enum.into(Keyword.new)
+    [switches: switches, aliases: aliases]
+  end
+  def opt_pair(_) do
+    []
+  end
 
-    def opt_pair({_name, args, _guards}) do
-        switches = args
-        |> Enum.map(&Firex.arg_name/1)
-        |> Enum.map(fn name -> {name, :string} end)
-        |> Enum.into(Keyword.new)
-        aliases = switches
-        |> Enum.map(fn {k, _} -> {Atom.to_string(k)
-                                    |> String.at(0)
-                                    |> String.to_atom(), k} end)
-        |> Enum.into(Keyword.new)
-        [switches: switches, aliases: aliases]
-    end
-    def opt_pair(_) do
-      []
-    end
+  def arg_name({:\\, _line, [{name, _, nil}, _default]}) do
+    name
+  end
+  def arg_name({name, _line, _}) do
+    name
+  end
 
-    def arg_name({:\\, _line, [{name, _, nil}, _default]}) do
-      name
-    end
-    def arg_name({name, _line, _}) do
-      name
-    end
-
-    def main(args \\ []) do
-        # require Firex.Main
-        # Firex.Main.main(args)
-    end
+  def main(args \\ []) do
+    # require Firex.Main
+    # Firex.Main.main(args)
+  end
 end
 
 
 defmodule Firex.Cli do
-    use Firex
+  use Firex
 
-    @spec launch(String.t, Bool.t) :: String.t
-    def launch(message, path, force \\ false) when is_binary(message) do
-        IO.puts "Hallo #{message} and #{path}"
-    end
+  @spec launch(String.t, Bool.t) :: String.t
+  def launch(message, path, force \\ false) when is_binary(message) do
+    IO.puts "Hallo #{message} and #{path}"
+  end
 
 end
--- mix.exs
+++ mix.exs
@@ -3,12 +3,12 @@ defmodule Firex.Mixfile do
 
   def project do
     [app: :firex,
-     version: "0.1.0",
-     elixir: "~> 1.3",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     escript: [main_module: Firex],
-     deps: deps()]
+    version: "0.1.0",
+    elixir: "~> 1.3",
+    build_embedded: Mix.env == :prod,
+    start_permanent: Mix.env == :prod,
+    escript: [main_module: Firex],
+    deps: deps()]
   end
 
   # Configuration for the OTP application
@@ -16,23 +16,23 @@ defmodule Firex.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger],
-    ]
-  end
+  ]
+end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
-  defp deps do
-    [
-     {:blacksmith, "~> 0.1"},
-     {:credo, only: ~w(test dev)a},
-     {:mix_test_watch, "~> 0.2.5", only: ~w(dev test)a},
-    ]
-  end
+# Dependencies can be Hex packages:
+#
+#   {:mydep, "~> 0.3.0"}
+#
+# Or git/path repositories:
+#
+#   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
+#
+# Type "mix help deps" for more examples and options
+defp deps do
+  [
+    {:blacksmith, "~> 0.1"},
+    {:credo, only: ~w(test dev)a},
+    {:mix_test_watch, "~> 0.2.5", only: ~w(dev test)a},
+  ]
+end
 end
